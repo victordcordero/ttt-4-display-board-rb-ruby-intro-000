a6a4a82f5d1ea350a99eb1f1d54b615ad460b73b
Victor Cordero <victordcordero@gmail.com>
2020-07-06 15:46:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,11 +12,11 @@ display_board(board)
 
 
 def display_board(board_one)
-  puts " #{board_one[0]} |  #{board_one[1]} | #{board_one[2]} "
+  puts " #{board_one[0]} | #{board_one[1]} | #{board_one[2]} "
   puts "-----------"
-  puts " #{board_one[3]} |  #{board_one[4]}  | #{board_one[5]} "
+  puts " #{board_one[3]} | #{board_one[4]} | #{board_one[5]} "
   puts "-----------"
-  puts " #{board_one[6]} | #{board_one[7]}  | #{board_one[8]} "
+  puts " #{board_one[6]} | #{board_one[7]} | #{board_one[8]} "
 end
 board_one = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 display_board(board_one)
